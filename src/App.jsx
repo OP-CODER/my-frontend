e46a6313ecd80c040c852f75264d8ea339a1f281
Anas <er.mohdanas974@gmail.com>
2025-08-01 17:15:58 +0530

    Added app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import ItemsList from './ItemsList';  // Import from separate file
+import React, { useEffect, useState } from 'react';
+import ItemsList from './ItemsList';
 
 function App() {
+  const [backendStatus, setBackendStatus] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8000/status')
+      .then(res => res.json())
+      .then(data => setBackendStatus(data.status))
+      .catch(err => setBackendStatus('Failed to fetch backend status'));
+  }, []);
+
   return (
     <div>
       <h1>My React App</h1>
-      {/* Render the ItemsList component */}
+      <p><em>{backendStatus}</em></p>
       <ItemsList />
     </div>
   );
